63ab1294e6fa5fe951d1cf6d80fa4f8e661aca16
Scott Moore <sm91srjc@gmail.com>
2022-06-09 19:38:13 -0700

    hashing passwords

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -4,7 +4,9 @@ const { User } = require("../../models");
 // GET /api/Users
 router.get("/", (req, res) => {
   // Accessour User model and run .findAll() method)
-  User.findAll()
+  User.findAll({
+    attributes: { exclude: ["password"] },
+  })
     .then((dbUserData) => res.json(dbUserData))
     .catch((err) => {
       console.log(err);
@@ -15,6 +17,7 @@ router.get("/", (req, res) => {
 // Get /api/users/1
 router.get("/:id", (req, res) => {
   User.findOne({
+    attributes: { exclude: ["password"] },
     where: {
       id: req.params.id,
     },
@@ -54,6 +57,7 @@ router.put("/:id", (req, res) => {
   // if req.body has exact key/value pairs to match the model, you can just use 'req.doy' instead
 
   User.update(req.body, {
+    individualHooks: true,
     where: {
       id: req.params.id,
     },
